3442c478cd6c1e0fccf33a24b633258cdfdc76fa
Vladislav Nedosekin <mr.vladis@gmail.com>
2021-05-27 17:27:06 +0100

    stack execution file

--- stack.sh
+++ stack.sh
@@ -5,4 +5,9 @@ aws --region eu-west-1 cloudformation update-stack --stack-name VPCAPI --templat
 aws --region eu-west-1 cloudformation create-stack --stack-name VPCConsumer --template-body file://cloudformation/vpc/vpc.yaml --parameters file://cloudformation/vpc/vpc_param_consumer.json
 aws --region eu-west-1 cloudformation update-stack --stack-name VPCConsumer --template-body file://cloudformation/vpc/vpc.yaml --parameters file://cloudformation/vpc/vpc_param_consumer.json
 
-# 
+# NAT
+aws --region eu-west-1 cloudformation create-stack --stack-name NATVPCAPIZA --template-body file://cloudformation/vpc/vpc-nat-gateway.yaml --parameters file://cloudformation/vpc/vpc-nat-gateway_param_VPCAPI_A.json
+aws --region eu-west-1 cloudformation update-stack --stack-name NATVPCAPIZA --template-body file://cloudformation/vpc/vpc-nat-gateway.yaml --parameters file://cloudformation/vpc/vpc-nat-gateway_param_VPCAPI_A.json
+aws --region eu-west-1 cloudformation create-stack --stack-name NATVPCConsumerZA --template-body file://cloudformation/vpc/vpc-nat-gateway.yaml --parameters file://cloudformation/vpc/vpc-nat-gateway_param_VPCConsumer_A.json
+aws --region eu-west-1 cloudformation update-stack --stack-name NATVPCConsumerZA --template-body file://cloudformation/vpc/vpc-nat-gateway.yaml --parameters file://cloudformation/vpc/vpc-nat-gateway_param_VPCConsumer_A.json
+
